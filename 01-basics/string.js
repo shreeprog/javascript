3a78a4537e94f6ff2828651fe72dddfd29d23edb
shreeprog <151999941+shreeprog@users.noreply.github.com>
2023-12-04 10:53:25 +0000

    file loaded

---
+++ 01-basics/string.js
@@ -0,0 +1,7 @@
+const name=new String("Bhubaneswar");
+console.log(name);
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.indexOf('b'));
+console.log(name.charAt(7));
