583b218fc6324d54974c093c1faba6ae2cbc0938
shreeprog <151999941+shreeprog@users.noreply.github.com>
2023-12-05 09:31:58 +0000

    file to be loaded.

---
+++ 01-basics/arraymethod.js
@@ -0,0 +1,17 @@
+const myarr=[1,2,3,4,5];
+console.log(myarr[4]);
+//Array prototype.push()->
+//it is used to add a element in the last of the array.
+console.log(myarr.push(6));
+//Array prototype.pop()->
+//it is used to remove the last element of the array.
+console.log(myarr.pop());
+console.log(myarr);
+//unshift()->
+//it is used to add element in the first index of array.
+console.log(myarr.unshift(14));
+console.log(myarr);
+//shift->
+//it is used to remove an element from the begining of an array.
+console.log(myarr.shift()); 
+console.log(myarr);
