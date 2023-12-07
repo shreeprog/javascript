ea51fe41c8d0458232df1bfdf8c6716bbec5f933
shreeprog <151999941+shreeprog@users.noreply.github.com>
2023-12-07 10:15:18 +0000

    file to be loaded.

---
+++ 01-basics/function.js
@@ -0,0 +1,14 @@
+//function
+function name()//function defination
+{
+console.log("s");//function body or scope
+console.log("u");
+console.log("b");
+console.log("h");
+console.log("a");
+}
+//function defination:where we define a function using function keyword and we give a name to a function.
+//curly braces are called function scope where the function visibility and life lies and we write the piece of code inside the function.
+//function call or excution: function call or excution is the most important part.because without calling a function a function cannot excute. 
+name();
+name();//function call
