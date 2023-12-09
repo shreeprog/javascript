a410d5715dac88f9b7ba5b60197a3b5e0b3f6b50
shreeprog <151999941+shreeprog@users.noreply.github.com>
2023-12-09 09:24:35 +0000

    file to be loaded.

--- 01-basics/arrowfunction.js
+++ 01-basics/arrowfunction.js
@@ -1,9 +1,29 @@
 one(5);
 function one(count){
-console.log(count+1);
-}
-
-const two=(count)=>{
     console.log(count+1);
-}
-two(8);
+    }
+    //one(5);
+    //two(8);//hoisting :in arrow function we cannot do call before function defination otherwise it will give error.
+    const two=(count)=>{
+        console.log(count+1);
+    }
+    two(8);
+    //const a;
+    //console.log(a);
+    //a=10;
+
+    //const sum(variable)//function name=(parameter)=>{
+        //function body
+    //}
+    //Sum(argument)//function call
+    //IIFE(Immediate Involve Function Execution)
+    (function passMessage(){
+console.log("first execution");
+console.log("database fetched");
+    })();  //so here semicolon is mandate.
+
+    (
+        (name)=>{
+        console.log(`${name},2nd IIFE arrow function execution`);
+   }
+   )(`shree`);
