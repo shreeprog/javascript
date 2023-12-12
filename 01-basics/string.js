eeadd3d04b5b2baafa786ac52ebe3ea288cb592f
shreeprog <151999941+shreeprog@users.noreply.github.com>
2023-12-12 09:58:03 +0000

    file to be loaded.

--- 01-basics/string.js
+++ 01-basics/string.js
@@ -5,3 +5,10 @@ console.log(name.toUpperCase());
 console.log(name.toLowerCase());
 console.log(name.indexOf('b'));
 console.log(name.charAt(7));
+//string coresion=num+string is string coresion
+//ex-let num=5
+//let string="six"
+//(5string)
+//js is dynamic programming language.the type of variable wiil bw check in complie time so is a dynamic language.
+//iife-immidently invoked function execution-
+//anonymous function-give no any name.
